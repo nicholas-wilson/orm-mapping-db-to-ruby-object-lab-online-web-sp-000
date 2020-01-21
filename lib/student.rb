ec55776e55e4297857e98b02fa20f9f88860474d
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-01-21 00:40:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -75,13 +75,13 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(number_of_students)
     sql = <<-SQL
       SELECT * FROM students
       WHERE grade = 10
-      LIMIT 10
+      LIMIT ?
     SQL
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, number_of_students).map do |row|
       self.new_from_db(row)
     end
   end
